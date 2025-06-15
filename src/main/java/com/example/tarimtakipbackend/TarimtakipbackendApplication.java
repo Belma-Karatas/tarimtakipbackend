4ad03a20e8b416a97c239038cc8fb96711bd487c
bilgesucakirr <22misy1016@isik.edu.tr>
2025-06-15 18:07:25 +0300

    İlk commit: Tarım Takip Sistemi eklendi

---
+++ src/main/java/com/example/tarimtakipbackend/TarimtakipbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarimtakipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarimtakipbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarimtakipbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tarimtakipbackend/TarimtakipbackendApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+
+package com.example.tarimtakipbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarimtakipbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+*/
